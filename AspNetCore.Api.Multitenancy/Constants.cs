0da5fdface7cf224e279f01c768730674cb18970
smuthiya <sachin_muthiyan@hotmail.com>
2021-02-13 15:47:51 +0530

    Added basic middleware

---
+++ AspNetCore.Api.Multitenancy/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Api.Multitenancy
+{
+    public static class Constants
+    {
+        public static string TenantKey = "tenant";
+    }
+}
---
+++ AspNetCore.Api.Multitenancy/Tenant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Api.Multitenancy
+{
+    public class Tenant
+    {
+        public string Id { get; set; }
+        public string Identifier { get; set; }
+    }
+}
